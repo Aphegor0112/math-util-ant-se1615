40fb3d3412bd3b46f15e3667ef1604ecd544c6ef
Aphegor0112 <duc2002nt@gmail.com>
2022-10-10 16:36:51 +0700

    apply getf() to recursive method

--- src/com/aphegor/mathutil/core/MathUtil.java
+++ src/com/aphegor/mathutil/core/MathUtil.java
@@ -17,18 +17,40 @@ public class MathUtil {
     //vì giai thừa tăng cực nhanh, 21! tràn kiểu long (18 số 0)
     //ko có giai thừa âm
     //quy ước: n nhận vào từ 0...20!
+    
+    //viết tính giai thừa theo đệ quy
+    //n!= 1.2.3.4....n
+    //c1: viết kiểu for, nhồi dần các kết quả vào 1 biến
+    //product = product * i; //i chạy từ 1...n
+    //c2: đệ quy - recurssion
+    //gọi lại chính mình với 1 một quy mô/giá trị khác
+    //búp bê của người Nga: con búp bê bự chứa con búp bê nhỏ bên trong
+    //                      con trong chứa con khác... khoảng 12 con
+    //                      lặp lại chính mình 12 lần
+    //5! = ??? 1.2.3.4.5
+    //5! = 4! . 5 = 5 x 4!
+    //4! = 4 x 3!
+    //n! = n x (n - 1)!
     public static long getFactorial(int n){
+        
         if (n < 0|| n > 20)
             throw new IllegalArgumentException("Invalid argument. n must be between 0...20");
         
         if (n==0 || n ==1)
             return 1; //tình huống đặc biệt, tính luôn
         
-        long product = 1; //tích khởi đầu là 1, biến tích lũy acc
-        for(int i =2; i<=n; i++)
-            product *= i; //product = product*i
+        //sống sót đến đây n = 2...20
+        //hàm này đang tính n! mà n! = n* (n - 1)!
         
-        return product;
+        return n * getFactorial(n - 1);
+        
+        //n! = n * (n - 1)!
+//        
+//        long product = 1; //tích khởi đầu là 1, biến tích lũy acc
+//        for(int i =2; i<=n; i++)
+//            product *= i; //product = product*i
+//        
+//        return product;
     }
     
     //tư duy viết code theo kiểu TDD - Test Driven Development (có thể viết vào CV)
